661eb68727c80036a45cb6939c93c4204361b6a3
Ben Evans <ben@bensbit.co.uk>
2012-10-23 18:48:12 +0100

    Initial Commit

---
+++ addlinks.js
@@ -0,0 +1,14 @@
+
+//var HNCacheURL = "http://hncache.bensbit.co.uk/";
+var HNCacheURL = "http://hncache.bensbit.co.uk/";
+
+for (var i = 0; i < (30*3); i+=3) {
+	var n = i - 1;
+	var commentsLink = $('table table tbody tr:nth-of-type('+n+') td.subtext span').attr('id');
+	//var ID = commentsLink.substring(6);
+	if (typeof commentsLink == 'string') {
+		var ID = commentsLink.substr(6, 10);
+	};
+	
+	$('table table tbody tr:nth-of-type('+n+') td.subtext').append(" | <a href=\"" + HNCacheURL + ID + "\">Cached</a>");
+};
